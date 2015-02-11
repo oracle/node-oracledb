56c8b60158190b6eae19d50077ab3f75ba206256
Christopher Jones <christopher.jones@oracle.com>
2015-02-11 15:26:01 -0800

    Bump version to 0.3.1

--- src/njs/src/njsOracle.h
+++ src/njs/src/njsOracle.h
@@ -40,9 +40,9 @@ using namespace node;
 using namespace v8; 
 
 
-/*0.2.4.  Keep the version in sync with package.json */
+/*0.3.1.  Keep the version in sync with package.json */
 /* Formula: 10000 x majorversion + 100 * minorversion + patchrelease number */
-#define NJS_ORACLE_VERSION 204
+#define NJS_ORACLE_VERSION 301
 
 
 class Oracledb: public ObjectWrap 
