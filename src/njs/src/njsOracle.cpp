oracle/node-oracledb

commit d3bc2cdaaf3e7f91b0d671c51a88a78fc9492bbe

DPI changes (charset/ncharset settable, heterogeneous pool, SYSDBA connections, session tagging). These changes are not exposed in node-oracledb

src/njs/src/njsOracle.cpp
@@ -791,7 +791,8 @@ void Oracledb::Async_GetConnection (uv_work_t *req)
                                               connBaton->connStr,
                                               connBaton->stmtCacheSize,
                                               connBaton->connClass,
-                                              connBaton->externalAuth );
+                                              connBaton->externalAuth,
+                                              dbPrivNONE );
 
     connBaton->dpiconn->lobPrefetchSize(connBaton->lobPrefetchSize);
   }

src/dpi/include/dpiConn.h
@@ -46,6 +46,14 @@ namespace dpi
 /*---------------------------------------------------------------------------
                      PUBLIC CONSTANTS
   ---------------------------------------------------------------------------*/
+// Enumeration for Database Privileges
+typedef enum
+{
+  dbPrivNONE = 0,                                             // None specified
+  dbPrivSYSDBA,                                                       // SYSDBA
+} DBPrivileges;
+
+
 
 /*---------------------------------------------------------------------------
                      PUBLIC TYPES
@@ -58,7 +66,7 @@ class Conn
 public:
                                 // termination
 
-  virtual void release() = 0;
+  virtual void release( const string &tag = "", boolean retag = false ) = 0;
 
                                 // properties
   virtual void stmtCacheSize(unsigned int stmtCacheSize) = 0;
@@ -74,6 +82,10 @@ public:
   virtual void module(const string &module) = 0;
 
   virtual void action(const string &action) = 0;
+
+  // In case of pooled-connections & tagged sessions, did we get session
+  // with provided Tag
+  virtual boolean sameTag () = 0;
 
                                 // methods
   virtual Stmt* getStmt (const string &sql="") = 0;

src/dpi/include/dpiEnv.h
@@ -66,7 +66,9 @@ class Env
  public:
                                 // creation/termination
 
-  static Env * createEnv( const string &drvName = "" );
+  static Env * createEnv( const string &drvName,
+                          unsigned int charset = 0,
+                          unsigned int ncharset = 0);
 
   virtual void terminate() = 0;
 
@@ -86,6 +88,8 @@ class Env
   virtual void externalAuth(bool externalAuth) = 0;
   virtual bool externalAuth() const = 0;
 
+  virtual unsigned int dbcharset () const = 0;
+  virtual unsigned int dbncharset () const = 0 ;
 
                                  // methods
   virtual SPool * createPool(const string &user, const string &password,
@@ -94,17 +98,20 @@ class Env
                              int poolIncrement = -1,
                              int poolTimeout = -1,
                              int stmtCacheSize = -1,
-                             bool externalAuth = false) = 0;
+                             bool externalAuth = false,
+                             bool homogeneous = true ) = 0;
 
-  virtual Conn * getConnection(const string &user, const string &password,
+  virtual Conn * getConnection(const string &user,
+                               const string &password,
                                const string &connString,
                                int stmtCacheSize,
                                const string &connClass = "",
-                               bool externalAuth = false) = 0;
+                               bool externalAuth = false,
+                               DBPrivileges dbpriv = dbPrivNONE ) = 0;
 
                                 // DateTime array
   virtual DateTimeArray * getDateTimeArray( OCIError *errh ) const = 0;
-  virtual void            releaseDateTimeArray ( DateTimeArray *arr ) const = 0;
+  virtual void            releaseDateTimeArray ( DateTimeArray *arr ) const =0;
 
                                  // handle and descriptor methods
   virtual DpiHandle * allocHandle(HandleType handleType) = 0;

src/dpi/include/dpiException.h
@@ -50,9 +50,9 @@ enum DpiError                          // error type
   DpiErrInvalidState,
   DpiErrUninitialized,
   DpiErrExtAuth,
-  DpiOciInvalidHandle,
-  DpiErrMemAllocFail,
-  DpiErrNullValue,
+  DpiOciInvalidHandle,  // "Invalid OCI Handle/Descriptor or invalid parameter for OCI handle/descriptor allocation call"
+  DpiErrMemAllocFail,   // "Memory allocation failed"
+  DpiErrNullValue,      // "Unexpected NULL value"
 };
 
 

src/dpi/include/dpiPool.h
@@ -59,7 +59,12 @@ class SPool
 
 
                                 // methods
-  virtual Conn * getConnection( const std::string &connClass = "" ) = 0;
+  virtual Conn * getConnection( const std::string &connClass = "",
+                                const std::string &username = "",
+                                const std::string &password = "",
+                                const std::string &tag = "",
+                                const boolean any = false,
+                                const DBPrivileges dbPriv = dbPrivNONE ) = 0;
 
 
 protected:

src/dpi/src/dpiConnImpl.cpp
@@ -73,6 +73,8 @@ using namespace std;
      user          - userid
      password      - password
      connString    - connect string
+     connClass     - DRCP Connection class string
+     dbPriv        - DB Privileges (SYSDBA or none).
 
    RETURNS:
      nothing
@@ -81,16 +83,19 @@ using namespace std;
 ConnImpl::ConnImpl(EnvImpl *env, OCIEnv *envh, bool externalAuth,
                    unsigned int stmtCacheSize,
                    const string &user, const string &password,
-                   const string &connString, const string &connClass)
+                   const string &connString, const string &connClass,
+                   DBPrivileges dbPriv)
 
 try :  env_(env), pool_(NULL),
        envh_(envh), errh_(NULL), auth_(NULL), svch_(NULL), sessh_(NULL),
-       hasTxn_(false), srvh_(NULL), dropConn_(false)
+       hasTxn_(false), srvh_(NULL), dropConn_(false), tag_(""),
+       retag_(false), sameTag_ (false)
 {
 
   this->initConnImpl ( false, externalAuth, connClass,
                        ( OraText * ) connString.data (),
-                       ( ub4 ) connString.length (), user, password );
+                       ( ub4 ) connString.length (), user, password, "",
+                       false, dbPriv );
 
   this->stmtCacheSize(stmtCacheSize);
 }
@@ -113,25 +118,33 @@ catch (...)
      poolName        - name of the pool
      poolNameLen     - length of pool name
      connectionClass - connection class.
+     user            - username in case of non-homogenous pool
+     password        - password in case of non-homogenous pool
+     tag             - session tag name
+     matchAny        - Match Tag name as MATCHANY or EXACT
+     dbPriv          - DB privileges (SYSDBA or none).
 
    RETURNS:
      nothing
 
    NOTES:
      This constructor to be used in session-pool scenarios.
+     For homogeneous pool, user, password should be empty string.
  */
 
 ConnImpl::ConnImpl(PoolImpl *pool, OCIEnv *envh, bool externalAuth,
-                   OraText *poolName, ub4 poolNameLen, const string& connClass
-                   )
+                   OraText *poolName, ub4 poolNameLen, const string& connClass,
+                   const string &user, const string &password,
+                   const string &tag, const boolean matchAny,
+                   const DBPrivileges dbPriv )
 
 try :  env_(NULL), pool_(pool),
        envh_(envh), errh_(NULL), auth_(NULL),
        svch_(NULL), sessh_(NULL), hasTxn_(false), srvh_(NULL),
-       dropConn_(false)
+       dropConn_(false), tag_ (""), retag_ (false), sameTag_(false)
 {
   this->initConnImpl ( true, externalAuth, connClass, poolName, poolNameLen,
-                       "", "" );
+                       user, password, tag, matchAny, dbPriv );
 }
 
 catch (...)
@@ -169,7 +182,8 @@ ConnImpl::~ConnImpl()
      Release the connection.
 
    PARAMETERS:
-     none
+     tag    - session tag name
+     retag  - tagCLRTAG, tagNONE
 
    RETURNS:
      nothing
@@ -178,7 +192,7 @@ ConnImpl::~ConnImpl()
 
  */
 
-void ConnImpl::release()
+void ConnImpl::release( const string &tag, boolean retag )
 {
   #if OCI_MAJOR_VERSION >= 12
     ociCall(OCIAttrGet(sessh_, OCI_HTYPE_SESSION, &hasTxn_, NULL,
@@ -187,6 +201,13 @@ void ConnImpl::release()
 
   if(hasTxn_)
     rollback();
+
+  retag_ = retag;                       /* for later use */
+
+  if ( tag.length () > 0 )
+  {
+    tag_ = tag;
+  }
 
   if (pool_)
     pool_->releaseConnection(this);
@@ -564,6 +585,9 @@ unsigned int ConnImpl::getServerVersion ()
      poolNmRconnStr - poolName or connectString
      user           - userid in case of non-pool scenario
      password       - password in case of non-pool scenario
+     tag            - session tag name
+     any            - Match session tag name as MATCHANY or MATCHEXACT
+     dbPriv         - DB Privileges (SYSDBA or none)
 
    RETURNS:
      nothing
@@ -571,7 +595,8 @@ unsigned int ConnImpl::getServerVersion ()
 
 void ConnImpl::initConnImpl ( bool pool, bool externalAuth,
                    const string& connClass, OraText *poolNmRconnStr,
-                   ub4 nameLen, const string &user, const string &password )
+                   ub4 nameLen, const string &user, const string &password,
+                   const string &tag, const boolean any, DBPrivileges dbPriv )
 {
   ub4 mode        = OCI_DEFAULT;
   ub2 csid        = 0;
@@ -611,6 +636,18 @@ void ConnImpl::initConnImpl ( bool pool, bool externalAuth,
                            OCI_ATTR_PASSWORD, errh_ ), errh_ );
   }
 
+  switch ( dbPriv )
+  {
+    case dbPrivSYSDBA:
+      mode |= OCI_SESSGET_SYSDBA;
+      break;
+
+    case dbPrivNONE:
+    default:
+      break;
+  }
+
+
   // If connection class provided, set it on auth handle
   if ( connClass.length () )
   {
@@ -631,8 +668,25 @@ void ConnImpl::initConnImpl ( bool pool, bool externalAuth,
                            OCI_ATTR_DRIVER_NAME, errh_ ), errh_);
   }
 
+  /*
+   * Applicable only on Pooled sessions - attempts to return a session with
+   * specified tag
+   *
+   * If any is false - then a session with a different tag is never returned
+   * if any is true -  if such a session not available, available untagged
+   *                   if no untagged is available, then any tagged session
+   *                   is returned.  All returned sessions are authenticated.
+   *
+   * sameTag_ flag will be true if such a tagged session was returned
+   *                   false otherwise.
+   */
+  if ( pool && any )
+  {
+    mode |= OCI_SESSGET_SPOOL_MATCHANY;
+  }
+
   ociCall ( OCISessionGet ( envh_, errh_, &svch_, auth_, poolNmRconnStr,
-                          ( ub4 ) nameLen, NULL, 0, NULL, NULL, NULL,
+                          ( ub4 ) nameLen, NULL, 0, NULL, NULL, &sameTag_,
                           mode ), errh_ );
 
   ociCall ( OCIAttrGet ( svch_, OCI_HTYPE_SVCCTX, &sessh_,  0,
@@ -686,7 +740,21 @@ void ConnImpl::cleanup()
         relMode |= OCI_SESSRLS_DROPSESS;
     }
 
-    OCISessionRelease(svch_, errh_, NULL, 0, relMode);
+    // Re-tagging
+    if( retag_ )
+    {
+      relMode |= OCI_SESSRLS_RETAG;
+    }
+
+    /*
+     * RETAG behavior: (same as in OCI).
+     *  if retag_ is TRUE & tag_ length is non-zero, then tag_ is set
+     *  if retag_ is TRUE & tag_ length is zero, then session-tag is cleared
+     *  if retag_ is FALSE, then no action taken(if tag_ given will be ignored)
+     */
+
+    OCISessionRelease(svch_, errh_, (OraText *)tag_.c_str (),
+                      (ub4) tag_.length (), relMode);
     svch_ = NULL;
   }
 

src/dpi/src/dpiConnImpl.h
@@ -76,15 +76,17 @@ class ConnImpl : public Conn
            unsigned int stmtCacheSize,
            const string &user, const string &password,
            const string &connString,
-           const string &connClass);
+           const string &connClass,
+           DBPrivileges dbPriv );
 
   ConnImpl(PoolImpl *pool, OCIEnv *envh, bool externalAuth,
-           OraText *poolName, ub4 poolNameLen, const string &connClass
-           );
+           OraText *poolName, ub4 poolNameLen, const string &connClass,
+           const string &user, const string &password, const string &tag,
+           const boolean any, const DBPrivileges dbPriv );
 
   virtual ~ConnImpl();
 
-  virtual void release();
+  virtual void release( const string &tag, boolean retag);
 
                                 // interface properties
   virtual void stmtCacheSize(unsigned int stmtCacheSize);
@@ -98,6 +100,10 @@ class ConnImpl : public Conn
   virtual void module(const string &module);
 
   virtual void action(const string &action);
+
+  // Connection with requested tag returned or not?
+  virtual boolean sameTag ()      { return sameTag_;  }
+
   virtual int getByteExpansionRatio ();
 
                               // interface methods
@@ -136,7 +142,8 @@ private:
 
   void initConnImpl( bool pool, bool externalAuth, const string& connClass,
                      OraText *poolNmRconnStr, ub4 nameLen,
-                     const string &user, const string &password );
+                     const string &user, const string &password,
+                     const string &tag, boolean any, DBPrivileges dbPriv );
 
   int getCsRatio ( ub2 csid )
   {
@@ -159,6 +166,9 @@ private:
   int         csratio_;         // character expansion ratio
   OCIServer   *srvh_;           // OCI server handle
   bool        dropConn_;        // Set flag in case of unusable connection
+  string      tag_;             // Session tag
+  boolean     retag_;           // How to retag? (leave it, update, clear)
+  boolean     sameTag_;         // connection is of same tag as requested?
 };
 
 

src/dpi/src/dpiEnv.cpp
@@ -80,15 +80,17 @@ Env::~Env()
      Create the Env object.
 
    PARAMETERS:
-     none
+     drvName        - driver name
+     charset        - charset id
+     ncharset       - ncharset id
 
    RETURNS:
      nothing
  */
-
-Env * Env::createEnv( const string & drvName )
+Env * Env::createEnv( const string& drvName,
+                      unsigned int charset, unsigned int ncharset)
 {
-  return EnvImpl::createEnvImpl( drvName );
+  return EnvImpl::createEnvImpl( drvName, charset, ncharset );
 }
 
 

commit 0c62081654c318efb07ebba3d5b3863a3153ab4f

Fix character set initialization broken in d3bc2cd. Re-enable tests

src/njs/src/njsOracle.cpp
@@ -78,7 +78,8 @@ Nan::Persistent<FunctionTemplate> Oracledb::oracledbTemplate_s;
  */
 Oracledb::Oracledb()
 {
-  dpienv_                  = dpi::Env::createEnv( driverName() );
+  dpienv_                  = dpi::Env::createEnv( driverName(), DPI_AL32UTF8,
+                                                  DPI_AL32UTF8 );
   outFormat_               = NJS_ROWS_ARRAY;
   maxRows_                 = NJS_MAX_ROWS;
   autoCommit_              = false;

test/binding.js
@@ -629,7 +629,7 @@ describe('4. binding.js', function() {
             }
           );
         },
-        /*function(callback) {
+        function(callback) {
           connection.execute(
             "BEGIN nodb_testproc(:o); END;",
             [
@@ -643,7 +643,7 @@ describe('4. binding.js', function() {
               callback();
             }
           );
-        },*/
+        },
         function(callback) {
           connection.execute(
             "DROP PROCEDURE nodb_testproc",
@@ -668,7 +668,7 @@ describe('4. binding.js', function() {
       );
     })
 
-    it.skip('4.4.3 Negative - bind out data exceeds default length', function(done) {
+    it('4.4.3 Negative - bind out data exceeds default length', function(done) {
       connection.execute(
         "BEGIN :o := lpad('A',201,'x'); END;",
          { o: { type: oracledb.STRING, dir : oracledb.BIND_OUT } },