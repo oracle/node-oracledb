oracle/node-oracledb

commit 35594e79c047be7b323c2c3705f256be2b6aa1b7

Validate objects used in bind-by-position array and display NJS-044

src/njs/src/njsConnection.cpp
@@ -662,7 +662,7 @@ void Connection::GetBinds (Handle<Object> bindobj, eBaton* executeBaton)
     bind->key = ":"+std::string(str);
     Local<Value> val__ = bindobj->Get(Nan::New<v8::String>((char*)str.c_str(),
                            (int) str.length()).ToLocalChecked());
-    Connection::GetBindUnit(val__, bind, executeBaton);
+    Connection::GetBindUnit(val__, bind, false, executeBaton);
     if(!executeBaton->error.empty())
       goto exitGetBinds;
   }
@@ -689,7 +689,7 @@ void Connection::GetBinds (Handle<Array> binds, eBaton* executeBaton)
   {
     Bind* bind = new Bind;
     Local<Value> val__ = binds->Get(index);
-    GetBindUnit(val__, bind, executeBaton);
+    GetBindUnit(val__, bind, true, executeBaton);
     if(!executeBaton->error.empty()) goto exitGetBinds;
   }
   exitGetBinds:
@@ -702,9 +702,12 @@ void Connection::GetBinds (Handle<Array> binds, eBaton* executeBaton)
      Processing each bind varible
 
    PARAMETERS:
-     Handle value, eBaton struct
+     val          - handle value
+     bind         - one bind structure to initialize
+     array        - array input or JSON input
+     executeBaton - eBaton structure
 */
-void Connection::GetBindUnit (Local<Value> val, Bind* bind,
+void Connection::GetBindUnit (Local<Value> val, Bind* bind, bool array,
                                        eBaton* executeBaton)
 {
   Nan::HandleScope scope;
@@ -713,6 +716,46 @@ void Connection::GetBindUnit (Local<Value> val, Bind* bind,
   if(val->IsObject() && !val->IsDate() && !Buffer::HasInstance(val))
   {
     Local<Object> bind_unit = val->ToObject();
+
+    if ( array )
+    {
+      // In case of array binds, JSON objects are expected to be unnamed.
+      // Named json object gets confused as we look for "dir", "type",
+      // "maxSize"  key words, but "name" will not match.
+      // Array binds syntax
+      //    [ id, name, {type : oracledb.STRING, dir : oracledb.BIND_OUT}]
+      // the 3rd parameter is unnamed JSON object.
+      // [ id, n, { a: { type : oracledb.STRING, dir : oracledb.BIND_OUT} }]
+      // will fail now.
+      Local<Array> keys = bind_unit->GetOwnPropertyNames ();
+      if ( keys->Length () > 0 )
+      {
+        bool valid = false;
+
+        for ( unsigned int index = 0; !valid && ( index < keys->Length ()) ;
+            index ++ )
+        {
+          std::string key;
+
+          Local<String> temp = keys->Get (index).As<String> ();
+          NJSString ( key, temp );
+
+          if ( ( key.compare ( "dir" ) == 0 ) ||
+               ( key.compare ( "type" ) == 0 ) ||
+               ( key.compare ( "maxSize" ) == 0 ) )
+          {
+            valid = true;
+          }
+        }
+
+        if ( !valid )
+        {
+          executeBaton->error = NJSMessages::getErrorMsg ( errNamedJSON );
+          goto exitGetBindUnit;
+        }
+      }
+    }
+
     NJS_GET_UINT_FROM_JSON   ( dir, executeBaton->error,
                                bind_unit, "dir", 1, exitGetBindUnit );
     NJS_GET_UINT_FROM_JSON   ( bind->type, executeBaton->error,

test/binding.js
@@ -400,25 +400,22 @@ describe('4. binding.js', function() {
       );
     })
 
-    it.skip('4.2.2 array binding with mixing JSON should throw an error', function(done) {
+    it('4.2.2 array binding with mixing JSON should throw an error', function(done) {
       connection.execute(
         insert,
         param2,
         options,
         function(err, result) {
-          should.exist(err);  // pending to fix
-          result.rowsAffected.should.be.exactly(1);
-          //result.outBinds[0].should.eql([1]);
-          //console.log(result);
+          should.exist(err);
+          (err.message).should.startWith('NJS-044');
+          // NJS-044: named JSON object is not expected in this context
           connection.execute(
             "SELECT * FROM nodb_binding",
             [],
             options,
             function(err, result) {
               should.not.exist(err);
-              //console.log(result);
-              result.rows[0].ID.should.be.exactly(2);
-              result.rows[0].NAME.should.eql('changjie');
+              (result.rows).should.be.eql([]);
               done();
             }
           );

src/njs/src/njsConnection.h
@@ -357,7 +357,7 @@ private:
   static void GetOptions (Handle<Object> options, eBaton* executeBaton);
   static void GetBinds (Handle<Object> bindobj, eBaton* executeBaton);
   static void GetBinds (Handle<Array> bindarray, eBaton* executeBaton);
-  static void GetBindUnit (Local<Value> bindtypes, Bind* bind,
+  static void GetBindUnit (Local<Value> bindtypes, Bind* bind, bool array,
                            eBaton* executeBaton);
   static void GetInBindParams(Local<Value> v8val, Bind *bind, eBaton *executeBaton, BindType bindType);
   static void GetInBindParamsScalar(Local<Value> v8val, Bind *bind, eBaton *executeBaton, BindType bindType);

src/njs/src/njsMessages.cpp
@@ -77,9 +77,12 @@ static const char *errMsg[] =
   "NJS-038: maxArraySize value should be greater than 0", // errInvalidValueArrayBind
   "NJS-039: empty array is not allowed for IN bind", // errEmptyArray
   "NJS-040: connection request timeout",  // errConnRequestTimeout
+
   "NJS-041: cannot convert ResultSet to QueryStream after invoking methods", // errCannotConvertRsToStream
   "NJS-042: cannot invoke ResultSet methods after converting to QueryStream", // errCannotInvokeRsMethods
   "NJS-043: ResultSet already converted to QueryStream", // errResultSetAlreadyConverted
+  "NJS-044: named JSON object is not expected in this context", // errNamedJSON
+
 };
 
 string NJSMessages::getErrorMsg ( NJSErrorType err, ... )

src/njs/src/njsMessages.h
@@ -79,6 +79,7 @@ typedef enum
   errCannotConvertRsToStream,
   errCannotInvokeRsMethods,
   errResultSetAlreadyConverted,
+  errNamedJSON,
 
   // New ones should be added here
 

commit a9374f0367cda39f3fc90a81c81dba246c5c3912

Re-allow unamed objects containing "val" for IN binds (broken in 35594e7)

src/njs/src/njsConnection.cpp
@@ -724,14 +724,17 @@ void Connection::GetBindUnit (Local<Value> val, Bind* bind, bool array,
 
     if ( array )
     {
-      // In case of array binds, JSON objects are expected to be unnamed.
-      // Named json object gets confused as we look for "dir", "type",
-      // "maxSize"  key words, but "name" will not match.
-      // Array binds syntax
+      // In case of bind-by-position, JSON objects are expected to be unnamed.
+      // If JSON object is provided, we look for "dir", "type", "maxSize",
+      // "val"  key names.  If not found report error
+      // values provided as in Array syntax - working case.
       //    [ id, name, {type : oracledb.STRING, dir : oracledb.BIND_OUT}]
-      // the 3rd parameter is unnamed JSON object.
+      // In this example the 3rd parameter is unnamed JSON object.
+      //
       // [ id, n, { a: { type : oracledb.STRING, dir : oracledb.BIND_OUT} }]
-      // will fail now.
+      // will fail now.  In this example the third parameter JSON object has
+      // a key with name "a" and value as another JSON.  Incorrect syntax
+      //
       Local<Array> keys = bind_unit->GetOwnPropertyNames ();
       if ( keys->Length () > 0 )
       {
@@ -747,7 +750,8 @@ void Connection::GetBindUnit (Local<Value> val, Bind* bind, bool array,
 
           if ( ( key.compare ( "dir" ) == 0 ) ||
                ( key.compare ( "type" ) == 0 ) ||
-               ( key.compare ( "maxSize" ) == 0 ) )
+               ( key.compare ( "maxSize" ) == 0 ) ||
+               ( key.compare ( "val" ) == 0 ) )
           {
             valid = true;
           }

test/binding.js
@@ -777,4 +777,148 @@ describe('4. binding.js', function() {
 
     })
   })
+
+  // Test cases involving JSON value as input
+  describe ('4.7 Value as JSON named/unamed test cases', function () {
+    it ( '4.7.1 valid case when numeric values are passed as it is',
+       function (done ) {
+         var sql = "SELECT SYSDATE FROM DUAL WHERE :b = 1 and :c = 456 ";
+         var binds = [ 1, 456 ];
+
+         oracledb.getConnection (
+           dbConfig,
+           function (err, connection ){
+
+             should.not.exist ( err ) ;
+             connection.execute (
+               sql,
+               binds,
+               function ( err, result ) {
+                 should.not.exist ( err );
+                 done ();
+               }
+             );
+           });
+      });
+
+    it ( '4.7.2 Valid values when one of the value is passed as JSON ',
+       function (done ) {
+         var sql = "SELECT SYSDATE FROM DUAL WHERE :b = 1 and :c = 456 ";
+         var binds = [ 1, { val : 456 } ];
+
+         oracledb.getConnection (
+           dbConfig,
+           function (err, connection ){
+
+             should.not.exist ( err ) ;
+             connection.execute (
+               sql,
+               binds,
+               function ( err, result ) {
+                 should.not.exist ( err );
+                 done ();
+               } );
+           });
+      });
+
+    it ( '4.7.3 Valid test case when one of the value is passed as JSON ',
+      function (done ) {
+        var sql = "SELECT SYSDATE FROM DUAL WHERE :b = 1 and :c = 456 ";
+        var binds = [ {val :  1}, 456 ];
+
+         oracledb.getConnection (
+           dbConfig,
+           function (err, connection ){
+
+             should.not.exist ( err ) ;
+             connection.execute (
+               sql,
+               binds,
+               function ( err, result ) {
+                 should.not.exist ( err );
+                 done ();
+               } );
+           });
+      });
+
+    it ( '4.7.4 Valid Test case when both values are passed as JSON',
+      function (done ) {
+        var sql = "SELECT SYSDATE FROM DUAL WHERE :b = 1 and :c = 456 ";
+        var binds = [ {val : 1}, {val : 456 } ];
+
+         oracledb.getConnection (
+           dbConfig,
+           function (err, connection ){
+
+             should.not.exist ( err ) ;
+             connection.execute (
+               sql,
+               binds,
+               function ( err, result ) {
+                 should.not.exist ( err );
+                 done ();
+               } );
+           });
+      });
+
+    it ( '4.7.5 Invalid Test case when value is passed as named JSON',
+      function (done ) {
+        var sql = "SELECT SYSDATE FROM DUAL WHERE :b = 1 and :c = 456 ";
+        var binds = [ {val : 1}, { c: {val : 456 } } ];
+
+        oracledb.getConnection (
+          dbConfig,
+          function (err, connection ){
+            should.not.exist ( err ) ;
+            connection.execute (
+              sql,
+              binds,
+              function ( err, result ) {
+                should.exist ( err );
+                (err.message).should.startWith ( 'NJS-044');
+                done ();
+             } );
+         });
+      });
+
+    it ( '4.7.6 Invalid Test case when other-value is passed as named JSON',
+      function (done ) {
+        var sql = "SELECT SYSDATE FROM DUAL WHERE :b = 1 and :c = 456 ";
+        var binds = [ { b: {val : 1} }, {val : 456 } ];
+
+        oracledb.getConnection (
+          dbConfig,
+          function (err, connection ){
+            should.not.exist ( err ) ;
+            connection.execute (
+              sql,
+              binds,
+              function ( err, result ) {
+                should.exist ( err );
+                (err.message).should.startWith ( 'NJS-044');
+                done ();
+              } );
+         });
+      });
+
+    it ( '4.7.7 Invalid Test case when all values is passed as named JSON',
+      function (done ) {
+        var sql = "SELECT SYSDATE FROM DUAL WHERE :b = 1 and :c = 456 ";
+        var binds = [ { b: {val : 1} }, { c: {val : 456 } } ];
+
+        oracledb.getConnection (
+          dbConfig,
+          function (err, connection ){
+            should.not.exist ( err ) ;
+            connection.execute (
+              sql,
+              binds,
+              function ( err, result ) {
+                should.exist ( err );
+                (err.message).should.startWith ( 'NJS-044');
+                done ();
+              } );
+          });
+      });
+  });
 })