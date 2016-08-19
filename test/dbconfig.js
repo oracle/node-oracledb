a1f8b169705a6b7b20bf16279124e3da3e254617
Christopher Jones <christopher.jones@oracle.com>
2016-08-19 13:16:24 +1000

    Update credentials for externalAuth tests

--- test/dbconfig.js
+++ test/dbconfig.js
@@ -30,9 +30,15 @@
  *
  *****************************************************************************/
 
-module.exports = {
-  user          : process.env.NODE_ORACLEDB_USER || "hr",
-  password      : process.env.NODE_ORACLEDB_PASSWORD || "welcome",
-  connectString : process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost/orcl",
-  externalAuth  : process.env.NODE_ORACLEDB_EXTERNALAUTH ? true : false
-};
+if (process.env.NODE_ORACLEDB_EXTERNALAUTH)
+{
+  exports.externalAuth = true;
+}
+else
+{
+  exports.externalAuth = false;
+  exports.user         = process.env.NODE_ORACLEDB_USER || "hr";
+  exports.password     = process.env.NODE_ORACLEDB_PASSWORD || "welcome";
+}
+
+exports.connectString  = process.env.NODE_ORACLEDB_CONNECTIONSTRING || "localhost/orcl";
