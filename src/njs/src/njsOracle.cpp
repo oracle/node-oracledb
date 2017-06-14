233cefd8765858c4a914bd0b0ed3af900ba4c494
Christopher Jones <christopher.jones@oracle.com>
2017-06-14 12:19:08 +1000

    Sync copyright in njs/ files

--- src/njs/src/njsOracle.cpp
+++ src/njs/src/njsOracle.cpp
@@ -1,4 +1,4 @@
-/* Copyright (c) 2015, 2017 Oracle and/or its affiliates. All rights reserved. */
+/* Copyright (c) 2015, 2017, Oracle and/or its affiliates. All rights reserved. */
 
 /******************************************************************************
  *
