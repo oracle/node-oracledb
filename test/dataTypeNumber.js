a01e943c5e048ad2193d652ad130489ec49792a3
Christopher Jones <christopher.jones@oracle.com>
2018-02-06 14:04:34 +1100

    Tests for 64-bit Integers

--- test/dataTypeNumber.js
+++ test/dataTypeNumber.js
@@ -37,6 +37,7 @@ var oracledb = require('oracledb');
 var should   = require('should');
 var assist   = require('./dataTypeAssist.js');
 var dbConfig = require('./dbconfig.js');
+var async    = require('async');
 
 describe('26. dataTypeNumber.js', function() {
 
@@ -46,11 +47,7 @@ describe('26. dataTypeNumber.js', function() {
 
   before('get one connection', function(done) {
     oracledb.getConnection(
-      {
-        user:          dbConfig.user,
-        password:      dbConfig.password,
-        connectString: dbConfig.connectString
-      },
+      dbConfig,
       function(err, conn) {
         should.not.exist(err);
         connection = conn;
@@ -111,5 +108,174 @@ describe('26. dataTypeNumber.js', function() {
       assist.verifyNullValues(connection, tableName, done);
     });
   });
+
+  // GitHub issue 833
+  // https://github.com/oracle/node-oracledb/issues/833
+  describe('26.3 large integers that cannot fit inside a 32-bit integer', function() {
+
+    it('26.3.1 original case', function(done) {
+
+      var num = 999999999999;
+      async.series([
+        function(cb) {
+          var proc = "BEGIN \n" +
+                     "    DECLARE \n" +
+                     "        e_table_missing EXCEPTION; \n" +
+                     "        PRAGMA EXCEPTION_INIT(e_table_missing, -00942); \n" +
+                     "    BEGIN \n" +
+                     "        EXECUTE IMMEDIATE('DROP TABLE nodb_tab_bignum PURGE'); \n" +
+                     "    EXCEPTION \n" +
+                     "        WHEN e_table_missing \n" +
+                     "        THEN NULL; \n" +
+                     "    END; \n" +
+                     "    EXECUTE IMMEDIATE (' \n" +
+                     "        CREATE TABLE nodb_tab_bignum ( \n" +
+                     "            id       NUMBER NOT NULL, \n" +
+                     "            content  NUMBER(12, 0) \n" +
+                     "        ) \n" +
+                     "    '); \n" +
+                     "END; ";
+
+          connection.execute(
+            proc,
+            function(err) {
+              should.not.exist(err);
+              cb();
+            }
+          );
+        },
+        function(cb) {
+          var sql = "insert into nodb_tab_bignum (id, content) values (1, :n)";
+          connection.execute(
+            sql,
+            { n: num },
+            function(err) {
+              should.not.exist(err);
+              cb();
+            }
+          );
+        },
+        function(cb) {
+          var sql = "select content from nodb_tab_bignum where id = 1";
+          connection.execute(
+            sql,
+            function(err, result) {
+              should.not.exist(err);
+              should.strictEqual(
+                result.rows[0][0],
+                num
+              );
+              cb();
+            }
+          );
+        },
+        function(cb) {
+          var sql = "DROP TABLE nodb_tab_bignum PURGE";
+          connection.execute(
+            sql,
+            function(err) {
+              should.not.exist(err);
+              cb();
+            }
+          );
+        }
+      ], done);
+    });
+  }); // 26.3
+
+  /*
+   * The maximum safe integer in JavaScript is (2^53 - 1) i.e. 9007199254740992.
+   * The minimum safe integer in JavaScript is (-(2^53 - 1)).
+   * Numbers out of above range will be rounded.
+   */
+  describe('26.4 Large number, edge cases', function() {
+
+    it('26.4.1 maximum safe integer, (2^53 - 1)', function(done) {
+
+      var num = 9007199254740992;
+      var sql = "select " + num + " from dual";
+      connection.execute(
+        sql,
+        function(err, result) {
+          should.not.exist(err);
+          should.strictEqual(
+            result.rows[0][0],
+            num
+          );
+          done();
+        }
+      );
+    });
+
+    it('26.4.2 Negative - maximum safe integer + 1', function(done) {
+
+      var actual = '9007199254740993';
+      var expected = 9007199254740992;
+
+      var sql = "SELECT TO_NUMBER( " + actual + " ) FROM DUAL";
+      connection.execute(
+        sql,
+        function(err, result) {
+          should.not.exist(err);
+          var outNum = result.rows[0][0];
+          should.strictEqual(outNum, expected);
+          done();
+        }
+      );
+    });
+
+    it('26.4.3 minimum safe integer', function(done) {
+
+      var num = -9007199254740992;
+      var sql = "select " + num + " from dual";
+      connection.execute(
+        sql,
+        function(err, result) {
+          should.not.exist(err);
+          should.strictEqual(
+            result.rows[0][0],
+            num
+          );
+          done();
+        }
+      );
+    });
+
+    it('26.4.4 Negative - minimum safe integer - 1', function(done) {
+      var actual = '-9007199254740993';
+      var expected = -9007199254740992;
+
+      var sql = "SELECT TO_NUMBER( " + actual + " ) FROM DUAL";
+      connection.execute(
+        sql,
+        function(err, result) {
+          should.not.exist(err);
+          var outNum = result.rows[0][0];
+          should.strictEqual(outNum, expected);
+          done();
+        }
+      );
+    });
+
+    it('26.4.5 gets correct number via fetching as string', function(done) {
+      var num = '-9007199254740993';
+
+      var sql = "SELECT TO_NUMBER( " + num + " ) AS TS_NUM FROM DUAL";
+      connection.execute(
+        sql,
+        [],
+        {
+          fetchInfo : { "TS_NUM"  : { type : oracledb.STRING } }
+        },
+        function(err, result) {
+          should.not.exist(err);
+          var got = result.rows[0][0];
+          should.strictEqual(got, num);
+          done();
+        }
+      );
+    });
+
+  }); // 26.4
 
 });
