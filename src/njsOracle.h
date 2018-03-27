e832f2c18507cde15a23f569ce65e716f8a62217
Christopher Jones <christopher.jones@oracle.com>
2018-03-27 17:07:40 +1100

    Fix version merge issue

--- src/njsOracle.h
+++ src/njsOracle.h
@@ -70,8 +70,8 @@ using namespace v8;
 // The suffix should be something like "-dev" or "-beta.1".
 // For production, leave NJS_NODE_ORACLEDB_SUFFIX undefined (not "")
 #define NJS_NODE_ORACLEDB_MAJOR       2
-#define NJS_NODE_ORACLEDB_MINOR       1
-#define NJS_NODE_ORACLEDB_PATCH       2
+#define NJS_NODE_ORACLEDB_MINOR       2
+#define NJS_NODE_ORACLEDB_PATCH       0
 #define NJS_NODE_ORACLEDB_SUFFIX
 
 // define stringified version and driver name
