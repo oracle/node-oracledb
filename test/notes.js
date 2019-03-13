193d1e4d11ce6a7c7df3a0cdb088623cb1d5a637
Christopher Jones <sixd@php.net>
2019-03-13 11:00:17 +1100

    No need for Node.js v6 tests anymore

--- test/notes.js
+++ test/notes.js
@@ -1,21 +1,37 @@
+/* Copyright (c) 2018, 2019, Oracle and/or its affiliates. All rights reserved. */
+
+/******************************************************************************
+ *
+ * You may not use the identified files except in compliance with the Apache
+ * License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * The node-oracledb test suite uses 'mocha', 'should' and 'async'.
+ * See LICENSE.md for relevant licenses.
+ *
+ * NAME
+ *   notes.js
+ *
+ * DESCRIPTION
+ *   The prerequiste checks of test suite.
+ *
+ *****************************************************************************/
 'use strict';
 
 const oracledb = require('oracledb');
 const async = require('async');
 const dbconfig = require('./dbconfig.js');
 
-const nodeVersion = process.versions.modules;
-
-if (nodeVersion < 57) {
-  console.log("\n\n");
-  console.log("**************************************************************");
-  console.log("For Node.js v6, please run tests with command `npm run testv6`");
-  console.log("**************************************************************");
-  console.log("\n\n\n");
-}
-
 /****************** Verify the "user/password" provided by user **********************/
-
 const LOGTAG = "Global before-all Hook:\n";
 
 const configList = [
