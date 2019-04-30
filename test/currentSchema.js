36f9d7ac6b8467009127c9b2ca4fe7cd894500d7
Christopher Jones <sixd@php.net>
2019-04-30 18:06:23 +1000

    Add more currentSchema tests

---
+++ test/currentSchema.js
@@ -0,0 +1,87 @@
+/* Copyright (c) 2019, Oracle and/or its affiliates. All rights reserved. */
+
+/******************************************************************************
+ *
+ * You may not use the identified files except in compliance with the Apache
+ * License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * NAME
+ *   191. currentSchema.js
+ *
+ * DESCRIPTION
+ *   Test the "connection.currentSchema" property.
+ *
+ *****************************************************************************/
+'use strict';
+
+const oracledb  = require('oracledb');
+const should    = require('should');
+const dbconfig  = require('./dbconfig.js');
+const testsUtil = require('./testsUtil.js');
+
+describe('191. currentSchema.js', () => {
+
+  it('191.1 the value will be empty until it has been explicitly set', async () => {
+
+    try {
+      const conn = await oracledb.getConnection(dbconfig);
+      should.strictEqual(conn.currentSchema, '');
+
+      let schema = dbconfig.user;
+      conn.currentSchema = schema;
+      should.strictEqual(conn.currentSchema, schema);
+
+      await conn.close();
+    } catch(err) {
+      should.not.exist(err);
+    }
+
+  }); // 191.1
+
+  it('191.2 SQL alternative', async () => {
+
+    try {
+      const conn = await oracledb.getConnection(dbconfig);
+
+      let schema = dbconfig.user.toUpperCase();
+      let query = "ALTER SESSION SET CURRENT_SCHEMA = " + schema;
+      await conn.execute(query);
+      should.strictEqual(conn.currentSchema, schema);
+
+      await conn.close();
+    } catch(err) {
+      should.not.exist(err);
+    }
+
+  }); // 191.2
+
+  it('191.3 Negative - can not set inexistent schema', async () => {
+
+    async function setInvalidSchema() {
+      const conn = await oracledb.getConnection(dbconfig);
+
+      let schema = "foo";
+      conn.currentSchema = schema;
+
+      await conn.close();
+    }
+
+    await testsUtil.assertThrowsAsync(
+      async () => await setInvalidSchema(),
+      /ORA-01435/
+    );
+    // ORA-01435: user does not exist
+
+  }); // 191.3
+
+});
