f1cbdfc1fa49434e6b9056fd1aef73b0dab0a6dd
Slawomir Osoba <slawomir.osoba@gmail.com>
2022-03-15 15:55:32 +0100

    Add asyncStack tests

---
+++ test/asyncStack.js
@@ -0,0 +1,57 @@
+/* Copyright (c) 2015, 2021, Oracle and/or its affiliates. All rights reserved. */
+
+/******************************************************************************
+ *
+ * You may not use the identified files except in compliance with the Apache
+ * License, Version 2.0 (the "License.")
+ *
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * The node-oracledb test suite uses 'mocha' and node buil-in 'assert'.
+ * See LICENSE.md for relevant licenses.
+ *
+ * NAME
+ *   263. asyncStack.js
+ *
+ * DESCRIPTION
+ *   Test keeping a stacktrace in asynchronous methods.
+ *
+ *****************************************************************************/
+'use strict';
+
+const oracledb = require('oracledb');
+const {strictEqual, ok} = require('assert');
+
+describe('263. asyncStack.js', () => {
+
+  it('263.1 await getConnection()', async () => {
+    try {
+      await oracledb.getConnection({connectString:'unexists.example.com'});
+    } catch (e) {
+      strictEqual(e.errorNum, 12154); //  TNS:could not resolve the connect identifier specified
+      ok(e.stack.includes('asyncStack.js:37:22'), e.stack);
+    }
+  });
+
+  it('263.2 getConnection(cb)', (doneTest) => {
+    oracledb.getConnection({connectString:'unexists.example.com'}, (e, conn) => {
+      try {
+        ok(!conn, conn);
+        strictEqual(e.errorNum, 12154); //  TNS:could not resolve the connect identifier specified
+        ok(e.stack.includes('asyncStack.js:45:14'), e.stack);
+        doneTest();
+      } catch (e2) {
+        doneTest(e2);
+      }
+    });
+  });
+
+});
