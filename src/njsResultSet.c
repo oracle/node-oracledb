6f31c22a7ff8c2161bbdc4ed450a5f2c74da33af
Christopher Jones <christopher.jones@oracle.com>
2023-02-21 17:32:51 +1100

    Improve ResultSet metadata fetchtype handling

--- src/njsResultSet.c
+++ src/njsResultSet.c
@@ -328,11 +328,7 @@ static bool njsResultSet_setFetchTypes(napi_env env, njsResultSet *rs,
             case DPI_ORACLE_TYPE_CHAR:
             case DPI_ORACLE_TYPE_NCHAR:
             case DPI_ORACLE_TYPE_RAW:
-                if (var->dbTypeNum == DPI_ORACLE_TYPE_CLOB ||
-                        var->dbTypeNum == DPI_ORACLE_TYPE_NCLOB ||
-                        var->dbTypeNum == DPI_ORACLE_TYPE_BLOB) {
-                    var->maxSize = (uint32_t) -1;
-                } else if (var->maxSize == 0) {
+                if (var->maxSize == 0) {
                     var->maxSize = NJS_MAX_FETCH_AS_STRING_SIZE;
                 }
                 break;
